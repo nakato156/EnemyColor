f7655a9277684e334b53d4d060da7eca8c0ee2fc
nakato156 <nnakato150@gmail.com>
2022-02-07 16:14:12 -0500

    finish!

---
+++ README.md
@@ -0,0 +1,43 @@
+# Enemy Color
+Enemy Color es un algoritmo de regresión lineal diseñado para poder encontrar un contraste de un color (o su inverso).
+
+##Uso
+Despues de descargar el proyecto y añadirlo al suyo debe importarlo de esta forma
+```python
+from enemy_color import EnemyColor
+from numpy import array
+
+color = EnemyColor()
+color.predict(array([150, 130, 0]))
+```
+
+La clase `EnemyColor` puede recibir un parámetro `default_load` de valor booleano que especificará si se usa un modelo ya entrenado o se preparará uno para el entrenamiento.
+
+En caso desee entrenar el modelo desde 0 debe hacer lo siguiente:
+```python
+from enemy_color import EnemyColor
+from numpy import array
+
+color = EnemyColor(default_load=False)
+#cargamos los datos del csv por default
+color.load_data_csv()
+#entrenamos el modelo
+color.train(save=True)  #save indica si el modelo será guardado
+color.predict(array([100, 130, 0])) #predecimos
+```
+En caso quiera usar un set de datos propios deberá especificar la ruta, para ello debes cambiar el atributo `path_colors` que contiene la ruta del csv de colores utilizado para el modelo original. 
+
+```python
+from enemy_color import EnemyColor
+from numpy import array
+
+color = EnemyColor(default_load=False)
+color.path_colors = "ruta_de_tu_archio.csv"
+color.load_data_csv() #cargamos los datos (se usará la ruta proporcionada)
+#entrenamos el modelo
+color.train(save=True)  #save indica si el modelo será guardado
+color.predict(array([100, 130, 0])) #predecimos
+```
+El archivo `.csv` deberá tener 2 columnas una llamada `colors` y la otra `contrast`. En la columna `colors` se deberá especificar un color cualquiera en formato hexadecimal (sin el caracter `#`) y en la otra columna el contraste o inverso del color (igualmente en formato hexadecimal y sin el caracter `#`). Sin embargo puede proorcionar el color en un formato rgb y especificar como parámetro `is_hex` de la función `load_data_csv` donde se omitirá la conversión a RGB.
+
+Por defecto se le abrirá una ventana donde podrá ver el color resultante de la predicción. SI no desea verlo especifique el parámetro `preview=False` en el método `predict`.
---
+++ enemy_color.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+import cv2 as cv
+import joblib
+from PIL import ImageColor
+from importlib.util import find_spec
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+class EnemyColor():
+    def __init__(self, default_load=True):
+        self.model_rgb = {}
+        self.path_colors = "data/colors.csv"
+        self.path_models = "data/models"
+        if default_load: self.load_models()
+
+    def clasif_data_color(self, color)->dict:
+        rgb = {"r":[], "g":[], "b":[]}
+        for r,g,b in color:
+            rgb["r"].append(r)
+            rgb["g"].append(g)
+            rgb["b"].append(b)
+        return {k:np.array(v) for k,v in rgb.items()}
+    
+    def load_data_csv(self, is_hex=True):
+        df = pd.read_csv(self.path_colors)
+        if is_hex:
+            df["color"]= df["color"].apply(lambda x: ImageColor.getcolor(f"#{x}", "RGB"))
+            df["contrast"] = df["contrast"].apply(lambda x: ImageColor.getcolor(f"#{x}", "RGB"))
+
+        self.rgb_1 = self.clasif_data_color(df.color.to_list())
+        self.rgb_2 = self.clasif_data_color(df.contrast.to_list())
+        self.loaded = True
+        return self
+
+    def load_models(self):
+        self.model_rgb = {i: joblib.load(f'{self.path_models}/modelo_{i}.pkl') for i in "rgb"}
+        return self
+
+    def train(self, score=.25, save:bool=False)->dict:
+        for i in "rgb":
+            while True:
+                X_train, X_test, y_train, y_test = train_test_split(self.rgb_1[i], self.rgb_2[i], train_size=.9)
+                modelo = LinearRegression()
+                modelo.fit(X = X_train.reshape(-1, 1), y = y_train)
+                model_score = modelo.score(X_train.reshape(-1, 1), y_train)
+                if model_score>score: break
+                else: print(f"score: {model_score}")
+            if save: joblib.dump(modelo, f'{self.path_models}/modelo_{i}.pkl')
+            self.model_rgb[i] = modelo
+        return self.model_rgb
+
+    def predict(self, predict_color:np.array, preview=True)->tuple:
+        """
+        :predict_color: array de 3 elementos correspondiete a los canales rgb
+        Ejemplo:
+            predict(np.array([140, 250, 200]))
+        :preview: booleana que indica si se mostrará una previsualizacion del color
+        :return: una tupla de los canales rgb del color de contraste predicho
+        """
+        predict_rgb = {}
+        for i, c in enumerate("rgb"):
+            modelo = self.model_rgb[c]
+            predicciones = modelo.predict(X = predict_color[i].reshape(-1, 1))
+            predict_rgb[c] = [int(x) for x in predicciones[0:3,]]
+        self.rgb = tuple(x[0] for x in predict_rgb.values())
+        if preview: self.preview(*self.rgb)
+        return self.rgb
+    
+    def preview(self, *rgb):
+        if not rgb: rgb=self.rgb
+        prev_img = np.zeros((100,100,3),np.uint8)
+        for x in range(100):
+            for y in range(100):
+                prev_img[x,y] = [*rgb]
+        name = "MyImage"
+        cv.imwrite(f"{name}.png", prev_img)
+        
+        if not find_spec("matplotlib.pyplot") or find_spec("matplotlib.image"):
+            import matplotlib.pyplot as plt
+            import matplotlib.image as img
+
+        image = img.imread(f'{name}.png')
+        plt.imshow(image)
+        plt.show()
